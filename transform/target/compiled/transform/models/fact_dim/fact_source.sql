ff461c9121716ec74b27534eb4cf88652e25ff70
Selam Ayehubirhan <kabodshekinah@gmail.com>
2022-07-25 07:26:25 +0300

    Delete transform directory

--- transform/target/compiled/transform/models/fact_dim/fact_source.sql
+++
@@ -1,9 +0,0 @@
-
-with warehouse_data as (
-    select * from `DWH`.`elt`
-),
-final as (
-    SELECT cars, traveled_d, avg_speed, lat, lon, speed, lon_acc, lat_acc
-    FROM warehouse_data
-)
-select * from final
--- transform/target/run/transform/models/example/my_second_dbt_model.sql
+++
@@ -1,8 +0,0 @@
-
-  create view `DWH`.`my_second_dbt_model__dbt_tmp` as (
-    -- Use the `ref` function to select from other models
-
-select *
-from `DWH`.`my_first_dbt_model`
-where id = 1
-  );
