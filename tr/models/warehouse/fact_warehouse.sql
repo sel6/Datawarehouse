8446c74f2e43afa7c5642b4660691e2b9cc4b676
Selam Ayehubirhan <kabodshekinah@gmail.com>
2022-07-25 10:37:49 +0300

    Delete tr because its renamed

--- tr/models/warehouse/fact_warehouse.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-with source_data as (
-    select * from {{ source('traffic_source', 'elt') }}
-),
-final as (
-    SELECT cars, traveled_d, avg_speed, lat, lon, speed, lon_acc, lat_acc
-    FROM source_data
-)
-select * from final
