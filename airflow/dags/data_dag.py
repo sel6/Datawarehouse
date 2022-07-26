75f42e063b93f5019ddae9d3c053aee20622ea81
Selam Ayehubirhan <kabodshekinah@gmail.com>
2022-07-26 17:51:53 +0300

    Update and rename data.py to data_dag.py

--- airflow/dags/data_dag.py
+++ airflow/dags/data_dag.py
@@ -6,6 +6,10 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.operators.mysql_operator import MySqlOperator
 from airflow.operators.email_operator import EmailOperator
 import pandas as pd
+import os
+import sys
+print(os.path.abspath("includes.............................."))
+sys.path.append(os.path.abspath("includes"))
 
 from loader import Warehouse
 wr = Warehouse()
